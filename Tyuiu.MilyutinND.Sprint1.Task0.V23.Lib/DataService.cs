e8ac0ce71a02b08a4ba14d478eb520509b14495b
Tyuiu.milyutin@outlook.com <mm1llnikita@gmail.com>
2024-09-23 11:30:56 +0500

    Add project files.

---
+++ Tyuiu.MilyutinND.Sprint1.Task0.V23.Lib/DataService.cs
@@ -0,0 +1,13 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint1;
+
+namespace Tyuiu.MilyutinND.Sprint1.Task0.V23.Lib
+{
+    public class DataService : ISprint1Task0V23
+    {
+        public double Calculate()
+        {
+            //(15/5*4)+1
+            return (15 / 5 * 4) + 1;
+        }
+    }
+}
---
+++ Tyuiu.MilyutinND.Sprint1.Task0.V23.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+using Tyuiu.MilyutinND.Sprint1.Task0.V23.Lib;
+
+namespace Tyuiu.MilyutinND.Sprint1.Task0.V23.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            var res = ds.Calculate();
+            Assert.AreEqual(2, res);
+        }
+    }
+}
